7edaaf5740fa10835a101b60b1d8ec720abdb4e2
Maciej Wozniak <JuanX-G@github.com>
2025-09-03 07:42:46 +0200

    Improved cli, cleaned up folder structure, added logo.

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"GAF/internal/parser"
 	"GAF/internal/runer"
 	"GAF/pkg/scheduler"
+	"bufio"
 	"context"
 	"fmt"
 	"os"
@@ -41,7 +42,7 @@ func parseConf() map[string]string {
 	lines := strings.Split(string(f), "\n")
 	for i, v := range lines {
 		valKey := strings.Split(v, "=")
-		if len(valKey) < 2 {
+		if len(valKey) < 2 && v != "\n"{
 			fmt.Println("config error at line", i + 1)
 		} else {
 			vkMap[valKey[0]] = valKey[1]
@@ -50,7 +51,7 @@ func parseConf() map[string]string {
 	return vkMap
 }
 
-func runWrap(imap parser.IMaps) {
+func runWrap() {
 	filePath, err := PathLocalize("./configs/automations")
 
 	if err != nil {
@@ -90,7 +91,7 @@ func runWrap(imap parser.IMaps) {
 			},
 		}
 
-		err = r.Adder(string(afc), v.Name(), imap)
+		err = r.Adder(string(afc), v.Name(), iMap)
 		if err != nil {
 			fmt.Println("error adding automation:", err)
 			continue
@@ -146,7 +147,82 @@ func loadImports() parser.IMaps {
 	return rMap
 }
 
+func runByName() {
+	var userAName string
+	fmt.Println("what automatinon do you want to run")
+	fmt.Scan(&userAName)
+	filePath, err := PathLocalize("./configs/automations")
+
+	if err != nil {
+		panic(err)
+	}
+	err = os.Chdir(filePath)
+	aDir, err := os.ReadDir(".")
+	if err != nil {
+		fmt.Println("error occured reading the automations!", err)
+		os.Exit(1)
+	}
+	for _, v := range aDir {
+		name := v.Name()
+		nameS := strings.Split(name, "_")
+		if len(nameS) != 2 {
+			continue
+		}
+		if nameS[0] != "gaf" {
+			continue
+		}
+		aName := nameS[1]
+		aName = strings.Split(aName, ".")[0]
+		if aName == userAName {
+			fullName := fmt.Sprint("gaf_", aName, ".txt")
+			afc, err := os.ReadFile(fullName)
+			if err != nil {
+				fmt.Println("error reading the automation file!", err)
+				os.Exit(1)
+			}
+			r := &runer.Runner{}
+			err = r.Adder(string(afc), "printer", iMap)
+			if err != nil {
+				fmt.Println("error adding the automation")
+			}
+			err = r.Run()
+			if err != nil {
+				fmt.Println("error running the automation", err)
+			}
+
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			cChan := make(chan string)
+			fmt.Println("Press q AND then enter to stop")
+			go func() {
+				scanner := bufio.NewScanner(os.Stdin)
+				for scanner.Scan() {
+					text := scanner.Text()
+					if text == "q" {
+						cChan <- "q"
+						return
+					}
+				}
+			}()
+
+			go func() {
+				if <-cChan == "q" {
+					cancel()        // stop the runner
+					os.Exit(0)      // fully exit program
+				}
+			}()
+
+			err = r.Starter(ctx)
+			if err != nil {
+				fmt.Println("error running starter:", err)
+			}
+		}
+	}
+}
+
 var filePathStyle string
+var iMap parser.IMaps
 
 func main() {
 	confMap := parseConf()
@@ -162,7 +238,7 @@ func main() {
 	}
 	}
 
-	iMap := loadImports()
+	iMap = loadImports()
 	// in := bufio.NewReader(os.Stdin)
 	args := os.Args
 	switch len(args) {
@@ -171,59 +247,11 @@ func main() {
 		os.Exit(1)
 	case 2:
 		action := args[1]
-		switch action {
-		case "run":
-			var userAName string
-			fmt.Println("what automatinon do you want to run")
-			fmt.Scan(&userAName)
-			filePath, err := PathLocalize("./configs/automations")
-
-			if err != nil {
-				panic(err)
-			}
-			err = os.Chdir(filePath)
-			aDir, err := os.ReadDir(".")
-			if err != nil {
-				fmt.Println("error occured reading the automations!", err)
-				os.Exit(1)
-			}
-			for _, v := range aDir {
-				name := v.Name()
-				nameS := strings.Split(name, "_")
-				if len(nameS) != 2 {
-					continue
-				}
-				if nameS[0] != "gaf" {
-					continue
-				}
-				aName := nameS[1]
-				aName = strings.Split(aName, ".")[0]
-				if aName == userAName {
-					fullName := fmt.Sprint("gaf_", aName, ".txt")
-					afc, err := os.ReadFile(fullName)
-					if err != nil {
-						fmt.Println("error reading the automation file!", err)
-						os.Exit(1)
-					}
-					r := &runer.Runner{}
-					err = r.Adder(string(afc), "printer", iMap)
-					if err != nil {
-						fmt.Println("error adding the automation")
-					}
-					err = r.Run()
-					if err != nil {
-						fmt.Println("error running the automation", err)
-					}
-					r.Starter(context.Background())
-				}
-
-			}
-		case "run_all":
+		if action == "run" || action == "r" {
+			runByName()
+		} else if action == "runAll" || action == "ra" {
 			fmt.Println("Running all actions")
-			runWrap(iMap)
-		case "interp_all":
-
+			runWrap()
 		}
-
 	}
 }
